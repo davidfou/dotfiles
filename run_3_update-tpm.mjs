62af5e64d30c5b54d211efa05b62aa8bdc501b27
David Fournier <fr.david.fournier@gmail.com>
2022-03-06 12:41:39 +0100

    Fix update tpm when tmux is not running

--- run_3_update-tpm.mjs
+++ run_3_update-tpm.mjs
@@ -4,7 +4,11 @@ console.log(chalk.blue(`Running run_3_update-tpm...`));
 $.verbose = false;
 
 console.time("Done!");
-await $`~/.tmux/plugins/tpm/bin/install_plugins`;
-await $`~/.tmux/plugins/tpm/bin/update_plugins all`;
-await $`~/.tmux/plugins/tpm/bin/clean_plugins`;
+if (await $`tmux info`.exitCode === 0) {
+  await $`~/.tmux/plugins/tpm/bin/install_plugins`;
+  await $`~/.tmux/plugins/tpm/bin/update_plugins all`;
+  await $`~/.tmux/plugins/tpm/bin/clean_plugins`;
+} else {
+  console.log(chalk.yellow(`[!] tmux is not running`));
+}
 console.timeEnd("Done!");
