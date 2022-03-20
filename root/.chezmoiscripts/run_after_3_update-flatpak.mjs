348d20fd603082b65550ffe980b6ead86f8f26ac
David Fournier <fr.david.fournier@gmail.com>
2022-03-20 19:13:50 +0100

    No confirmation to update applications

--- root/.chezmoiscripts/run_after_3_update-flatpak.mjs
+++ root/.chezmoiscripts/run_after_3_update-flatpak.mjs
@@ -4,5 +4,5 @@ console.log(chalk.blue(`Running run_3_update-flatpak...`))
 $.verbose = false;
 
 console.time("Done!");
-await $`flatpak update`;
+await $`flatpak update -y`;
 console.timeEnd("Done!");
