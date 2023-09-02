b63b6bdd55ff71867e58844902305c808e28e041
David Fournier <fr.david.fournier@gmail.com>
2023-09-02 10:39:14 +0200

    Add Bitwarden

--- root/.chezmoiscripts/run_after_0_2_configure-asdf.sh
+++ root/.chezmoiscripts/run_after_0_2_configure-asdf.sh
@@ -39,3 +39,4 @@ install_plugin pulumi        https://github.com/canha/asdf-pulumi.git
 install_plugin golang        https://github.com/kennyp/asdf-golang.git
 install_plugin github-cli    https://github.com/bartlomiejdanek/asdf-github-cli.git
 install_plugin firebase      https://github.com/jthegedus/asdf-firebase.git
+install_plugin bitwarden     https://github.com/vixus0/asdf-bitwarden.git
