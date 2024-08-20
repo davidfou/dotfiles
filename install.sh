110e7b67743fb89feecdec126b363856dd12eb85
David Fournier <fr.david.fournier@gmail.com>
2024-08-20 22:36:01 +0200

    Copy only needed file for the first apply

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 set -e
 
 sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply davidfou
+. "$HOME/.asdf/asdf.sh"
+~/.local/bin/chezmoi apply
 
 echo 'Done! Time to restart the machine an run `chezmoi apply` in tmux!'
